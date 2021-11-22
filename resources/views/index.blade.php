940f9c951595efef5aea1aed240a230666fc9113
csetowhid <towhidhasang1@gmail.com>
2021-11-22 16:13:51 +0600

    popup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/app.css" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $("#myModal").modal('show');
+        });
+    </script>
 </head>
 <body>
+    <div class="modal" id="myModal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Enter Your Username</h5>
+            </div>
+            <div class="modal-body">
+                <input type="text" name="username" id="username" placeholder="Enter Username..." />
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     <div class="app">
         <header>
             <h1>Chat Application</h1>
-            <input type="text" name="username" id="username" placeholder="Enter Username..." />
+            {{-- <input type="text" name="username" id="username" placeholder="Enter Username..." /> --}}
         </header>
         <div id="messages"></div>
         <form id="message_form">
@@ -19,7 +42,7 @@
             <button type="submit" id="message_send">Send</button>
         </form>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./js/app.js"></script>
 </body>
 </html>
